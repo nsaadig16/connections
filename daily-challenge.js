77718097a9d6fe644152d3ee72f03c67c67f2f74
nsaadig16 <nsaadig16@gmail.com>
2025-04-27 19:56:23 +0200

    ammend daily challenge

--- daily-challenge.js
+++ daily-challenge.js
@@ -1,5 +1,5 @@
 // This file contains the daily challenge code
 const DAILY_CHALLENGE = {
-    code: "d29yayx0aW1lLGJsYWNrLHdpdGgKY3JlZWQsZGFuY2UsY3J5LGRhbmNlCmJvYXQsYmFsbG9vbixjb3JrLGNsb3VkCmNyb3AsYmx1cixyZXNpemUsZmlsdGVyCl9fX291dCxzZWNvbmQgd29yZHMgb2YgVWJpc29mdCBnYW1lcyx0aGluZ3MgdGhhdCBmbG9hdCx0aGluZ3MgeW91IGNhbiBkbyB0byBhIHBob3Rv",
+    code: "Ymx1cixmaWx0ZXIsY3JvcCxyZXNpemUKYmxhY2ssd29yayx0aW1lLHdpdGgKY29yayxiYWxsb29uLGNsb3VkLGJvYXQKZGFuY2UsY3J5LGNyZWVkLGRvZ3MKdGhpbmdzIHlvdSBjYW4gZG8gdG8gYW4gaW1hZ2UsX19fX291dCxmbG9hdHkgdGhpbmdzLDJuZCB3b3JkIG9mIFViaXNvZnQgZ2FtZXM=",
     date: "2025-04-27"
 };
